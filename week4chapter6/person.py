ae44b0182044ab980e5869860acd22a4a2a95b1a
Lisa Adams <redstarlarock75@gmail.com>
2023-09-20 20:56:38 -0400

    Completed person.py activity

--- week4chapter6/person.py
+++ week4chapter6/person.py
@@ -2,3 +2,15 @@
 #Store their first name, last name, age, and the city in which they live. You should have keys such as first_name, last_name, age, and city. #
 #Print each piece of information stored in your dictionary.
 
+person = {
+    'fname': 'John',
+    'lname': 'Counts',
+    'age': 35,
+    'city': "Pulaski"
+
+}
+
+print(person['fname'])
+print(person['lname'])
+print(person['age'])
+print(person['city'])
