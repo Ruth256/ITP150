3ba794a6c73f9a355b03768dd7ccce15bb28801f
Lisa Adams <redstarlarock75@gmail.com>
2023-09-27 22:04:02 -0400

    8-1 completed

---
+++ week5chapter8/message.py
@@ -0,0 +1,9 @@
+#8-1. Message: Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. 
+#Call the function, and make sure the message displays correctly.
+
+def display_message():
+    """function that displays a message"""
+    message= "I am learning about functions"
+    print(message)
+
+display_message()
