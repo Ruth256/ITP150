b19dead737e471e42b2d2ca83bfd6ff13d28dd36
Lisa Adams <redstarlarock75@gmail.com>
2023-09-27 22:14:53 -0400

    completed 8-2

---
+++ week5chapter8/favoriteMovies.py
@@ -0,0 +1,9 @@
+#8-2. Favorite Movie: Write a function called favorite_movie() that accepts one parameter, title. The function should print a message, such as One of my favorite movies is Alice in Wonderland. Call the function, making sure to include a movie title as an argument in the function call.
+
+def favorite_movies(title):
+    """Function to display my favorite movies"""
+    print(f"My favorite movie is {title}!")
+
+favorite_movies("Titanic")
+favorite_movies("Clueless")
+favorite_movies("Homeward Bound")
